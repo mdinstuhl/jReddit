43b19df77f28cf56ca0b1dec516bd214d8d0fa26
benjaminjakobus <benjaminjakobus@192.168.2.101>
2012-08-29 13:09:18 +0200

    Update: Added ability to retrieve user comments.

--- src/com/omrlnr/jreddit/user/User.java
+++ src/com/omrlnr/jreddit/user/User.java
@@ -10,15 +10,31 @@ import org.json.simple.parser.ParseException;
 
 import com.omrlnr.jreddit.Thing;
 import com.omrlnr.jreddit.utils.Utils;
+import java.util.List;
 
 /**
  * This class represents a user connected to reddit.
- * 
+ *
  * @author <a href="http://www.omrlnr.com">Omer Elnour</a>
  */
 public class User extends Thing {
+
 	private String username, password;
 	private String modhash, cookie;
+	// Comments made by this user
+	private List<Comment> comments;
+	// Links submitted by this user
+	private List<String> submissions;
+	// Links like by this user
+	private List<String> liked;
+	// Links disliked by this user
+	private List<String> disliked;
+	// Links hidden by this user
+	private List<String> hidden;
+	// How much karma this user has
+	private int karma;
+	// When the account was created
+	private String createdDate;
 
 	public User(String username, String password) {
 		this.username = username;
@@ -26,13 +42,11 @@ public class User extends Thing {
 	}
 
 	/**
-	 * Call this function to connect the user. <br />
-	 * By "connect" I mean effectively sending a POST request to reddit and
-	 * getting the modhash and cookie, which are required for most reddit API
-	 * functions.
-	 * 
-	 * @throws Exception
-	 *             If connection fails.
+	 * Call this function to connect the user. <br /> By "connect" I mean
+	 * effectively sending a POST request to reddit and getting the modhash and
+	 * cookie, which are required for most reddit API functions.
+	 *
+	 * @throws Exception If connection fails.
 	 */
 	public void connect() throws Exception {
 		ArrayList<String> hashCookiePair = hashCookiePair(username, password);
@@ -42,17 +56,12 @@ public class User extends Thing {
 
 	/**
 	 * This function submits a link to the specified subreddit.
-	 * 
-	 * @param title
-	 *            The title of the submission
-	 * @param link
-	 *            The link to the submission
-	 * @param subreddit
-	 *            The subreddit to submit to
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON Parsing fails
+	 *
+	 * @param title The title of the submission
+	 * @param link The link to the submission
+	 * @param subreddit The subreddit to submit to
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON Parsing fails
 	 */
 	public void submitLink(String title, String link, String subreddit)
 			throws IOException, ParseException {
@@ -67,24 +76,18 @@ public class User extends Thing {
 			System.err.println("That link has already been submitted.");
 		} else {
 			System.out.println("Link submitted to "
-					+ ((JSONArray) ((JSONArray) ((JSONArray) object
-							.get("jquery")).get(16)).get(3)).get(0));
+					+ ((JSONArray) ((JSONArray) ((JSONArray) object.get("jquery")).get(16)).get(3)).get(0));
 		}
 	}
 
 	/**
 	 * This function submits a self post to the specified subreddit.
-	 * 
-	 * @param title
-	 *            The title of the submission
-	 * @param text
-	 *            The text of the submission
-	 * @param subreddit
-	 *            The subreddit to submit to
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON Parsing fails
+	 *
+	 * @param title The title of the submission
+	 * @param text The text of the submission
+	 * @param subreddit The subreddit to submit to
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON Parsing fails
 	 */
 	public void submitSelfPost(String title, String text, String subreddit)
 			throws IOException, ParseException {
@@ -99,19 +102,16 @@ public class User extends Thing {
 			System.err.println("That link has already been submitted.");
 		} else {
 			System.out.println("Self post submitted to "
-					+ ((JSONArray) ((JSONArray) ((JSONArray) object
-							.get("jquery")).get(10)).get(3)).get(0));
+					+ ((JSONArray) ((JSONArray) ((JSONArray) object.get("jquery")).get(10)).get(3)).get(0));
 		}
 	}
 
 	/**
 	 * This functions returns true if this user has unread mail.
-	 * 
+	 *
 	 * @return This user has mail or not
-	 * @throws ParseException
-	 *             If JSON parsing fails
-	 * @throws IOException
-	 *             If connection fails
+	 * @throws ParseException If JSON parsing fails
+	 * @throws IOException If connection fails
 	 */
 	public boolean hasMail() throws IOException, ParseException {
 		return Boolean.parseBoolean(info().get("has_mail").toString());
@@ -119,14 +119,11 @@ public class User extends Thing {
 
 	/**
 	 * This function returns the Unix time that the user's account was created.
-	 * 
+	 *
 	 * @return Unix time that the user's account was created
-	 * @throws NumberFormatException
-	 *             If the "created" property isn't a double
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON parsing fails
+	 * @throws NumberFormatException If the "created" property isn't a double
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON parsing fails
 	 */
 	public double created() throws IOException, ParseException {
 		return Double.parseDouble(info().get("created").toString());
@@ -135,14 +132,12 @@ public class User extends Thing {
 	/**
 	 * This function returns the Unix time (in UTC/Coordinated Universal Time)
 	 * that the user's account was created.
-	 * 
+	 *
 	 * @return Unix time that the user's account was created in UTC
-	 * @throws NumberFormatException
-	 *             If the "created_utc" property isn't a double
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON parsing fails
+	 * @throws NumberFormatException If the "created_utc" property isn't a
+	 * double
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON parsing fails
 	 */
 	public double createdUTC() throws IOException, ParseException {
 		return Double.parseDouble(info().get("created_utc").toString());
@@ -152,14 +147,12 @@ public class User extends Thing {
 	 * This function returns the amount of link karma this user has. <br />
 	 * Returns int because I doubt anyone has more than 2,147,483,647 link
 	 * karma.
-	 * 
+	 *
 	 * @return Link Karma
-	 * @throws NumberFormatException
-	 *             If the "link_karma" property isn't an inteher
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON parsing fails
+	 * @throws NumberFormatException If the "link_karma" property isn't an
+	 * inteher
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON parsing fails
 	 */
 	public int linkKarma() throws IOException, ParseException {
 		return Integer.parseInt(info().get("link_karma").toString());
@@ -169,14 +162,12 @@ public class User extends Thing {
 	 * This function returns the amount of comment karma this user has. <br />
 	 * Returns int because I doubt anyone has more than 2,147,483,647 comment
 	 * karma.
-	 * 
+	 *
 	 * @return Comment Karma
-	 * @throws NumberFormatException
-	 *             If the "link_karma" property isn't an inteher
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON parsing fails
+	 * @throws NumberFormatException If the "link_karma" property isn't an
+	 * inteher
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON parsing fails
 	 */
 	public int commentKarma() throws IOException, ParseException {
 		return Integer.parseInt(info().get("comment_karma").toString());
@@ -184,12 +175,10 @@ public class User extends Thing {
 
 	/**
 	 * This functions returns true if this user has a gold account.
-	 * 
+	 *
 	 * @return This user has a gold account or not
-	 * @throws ParseException
-	 *             If JSON parsing fails
-	 * @throws IOException
-	 *             If connection fails
+	 * @throws ParseException If JSON parsing fails
+	 * @throws IOException If connection fails
 	 */
 	public boolean isGold() throws IOException, ParseException {
 		return Boolean.parseBoolean(info().get("is_gold").toString());
@@ -198,26 +187,22 @@ public class User extends Thing {
 	/**
 	 * This functions returns true if this user is a reddit moderator
 	 * (apparently this means a moderator of any subreddit).
-	 * 
+	 *
 	 * @return This user is a moderator or not
-	 * @throws ParseException
-	 *             If JSON parsing fails
-	 * @throws IOException
-	 *             If connection fails
+	 * @throws ParseException If JSON parsing fails
+	 * @throws IOException If connection fails
 	 */
 	public boolean isMod() throws IOException, ParseException {
 		return Boolean.parseBoolean(info().get("is_mod").toString());
 	}
 
 	/**
-	 * This function returns the user's ID. <br />
-	 * The user's ID. This is only used internally, <b>right</b>?
-	 * 
+	 * This function returns the user's ID. <br /> The user's ID. This is only
+	 * used internally, <b>right</b>?
+	 *
 	 * @return This user's ID
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON parsing fails
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON parsing fails
 	 */
 	public String id() throws IOException, ParseException {
 		return info().get("id").toString();
@@ -225,12 +210,10 @@ public class User extends Thing {
 
 	/**
 	 * This functions returns true if this user has unread moderator mail.
-	 * 
+	 *
 	 * @return This user has unread moderator mail or not
-	 * @throws ParseException
-	 *             If JSON parsing fails
-	 * @throws IOException
-	 *             If connection fails
+	 * @throws ParseException If JSON parsing fails
+	 * @throws IOException If connection fails
 	 */
 	public boolean hasModMail() throws IOException, ParseException {
 		return Boolean.parseBoolean(info().get("has_mod_mail").toString());
@@ -238,8 +221,8 @@ public class User extends Thing {
 
 	/**
 	 * Returns the user's username.
-	 * 
-	 * @return		The user's username.
+	 *
+	 * @return	The user's username.
 	 */
 	public String getUsername() {
 		return username;
@@ -247,8 +230,8 @@ public class User extends Thing {
 
 	/**
 	 * Returns the user's password.
-	 * 
-	 * @return		The user's password.
+	 *
+	 * @return	The user's password.
 	 */
 	public String getPassword() {
 		return password;
@@ -265,16 +248,12 @@ public class User extends Thing {
 	/**
 	 * This function logs in to reddit and returns an ArrayList containing a
 	 * modhash and cookie.
-	 * 
-	 * @param username
-	 *            The username
-	 * @param password
-	 *            The password
+	 *
+	 * @param username The username
+	 * @param password The password
 	 * @return An array containing a modhash and cookie
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If parsing JSON fails
+	 * @throws IOException If connection fails
+	 * @throws ParseException If parsing JSON fails
 	 */
 	private ArrayList<String> hashCookiePair(String username, String password)
 			throws IOException, ParseException {
@@ -282,8 +261,7 @@ public class User extends Thing {
 		JSONObject jsonObject = Utils.post("api_type=json&user=" + username
 				+ "&passwd=" + password, new URL(
 				"http://www.reddit.com/api/login/" + username), getCookie());
-		JSONObject valuePair = (JSONObject) ((JSONObject) jsonObject
-				.get("json")).get("data");
+		JSONObject valuePair = (JSONObject) ((JSONObject) jsonObject.get("json")).get("data");
 
 		values.add(valuePair.get("modhash").toString());
 		values.add(valuePair.get("cookie").toString());
@@ -294,13 +272,12 @@ public class User extends Thing {
 	/**
 	 * This function returns a "response" (me.json) JSON data containing info
 	 * about the user. <br />
-	 * 
+	 *
 	 * @return JSON data containing info about the user
 	 */
 	private JSONObject info() throws IOException, ParseException {
 		if (cookie == null || modhash == null) {
-			System.err
-					.printf("Please invoke the \"connect\" function before attempting to call any other API functions.");
+			System.err.printf("Please invoke the \"connect\" function before attempting to call any other API functions.");
 			Runtime.getRuntime().exit(-1);
 		}
 
@@ -312,20 +289,14 @@ public class User extends Thing {
 
 	/**
 	 * This function submits a link or self post.
-	 * 
-	 * @param title
-	 *            The title of the submission
-	 * @param linkOrText
-	 *            The link of the submission or text
-	 * @param selfPost
-	 *            If this submission is a self post
-	 * @param subreddit
-	 *            Which subreddit to submit this to
+	 *
+	 * @param title The title of the submission
+	 * @param linkOrText The link of the submission or text
+	 * @param selfPost If this submission is a self post
+	 * @param subreddit Which subreddit to submit this to
 	 * @return A JSONObject
-	 * @throws IOException
-	 *             If connection fails
-	 * @throws ParseException
-	 *             If JSON parsing fails
+	 * @throws IOException If connection fails
+	 * @throws ParseException If JSON parsing fails
 	 */
 	private JSONObject submit(String title, String linkOrText,
 			boolean selfPost, String subreddit) throws IOException,
@@ -334,5 +305,78 @@ public class User extends Thing {
 				+ "=" + linkOrText + "&sr=" + subreddit + "&kind="
 				+ (selfPost ? "link" : "self") + "&uh=" + getModhash(),
 				new URL("http://www.reddit.com/api/submit"), getCookie());
+	}
+
+	/**
+	 * Returns a list of comments made by this user.
+	 *
+	 * @return
+	 * <code>List</code> of comments made by this user.
+	 * @author Benjamin Jakobus
+	 */
+	public List<Comment> getComments() {
+		if (comments == null) {
+			comments = User.getComments(username, getCookie());
+		}
+		return comments;
+	}
+
+	/**
+	 * Returns a list of comments made by this user.
+	 *
+	 * @param username	The username of the user whose account overview you want
+	 * to retrieve.
+	 * @param cookie	The cookie associated with the account that you used to
+	 * connect to reddit.
+	 * @return
+	 * <code>List</code> of comments made by this user.
+	 *
+	 * @author Benjamin Jakobus
+	 */
+	public static List<Comment> getComments(String username, String cookie) {
+		// List of comments made by this user
+		List<Comment> comments = new ArrayList<Comment>(500);
+		try {
+			// Send GET request to get the account overview
+			JSONObject object = (JSONObject) Utils.get("", new URL(
+					"http://www.reddit.com/user/" + username + "/comments.json"), cookie);
+			JSONObject data = (JSONObject) object.get("data");
+			JSONArray children = (JSONArray) data.get("children");
+
+			JSONObject obj;
+			Comment c;
+			for (int i = 0; i < children.size(); i++) {
+				// Get the object containing the comment
+				obj = (JSONObject) children.get(i);
+				obj = (JSONObject) obj.get("data");
+
+				// Create a new comment
+				c = new Comment(toString(obj.get("body")), toString(obj.get("edited")),
+						toString(obj.get("created_utc")), toString(obj.get("replies")),
+						Integer.parseInt(toString(obj.get("ups"))),
+						Integer.parseInt(toString(obj.get("downs"))));
+				
+				// Add it to the comments list
+				comments.add(c);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Return the comments
+		return comments;
+	}
+	
+	/**
+	 * Safely converts an object into string (used because sometimes JSONObject's get() method returns
+	 * null).
+	 * 
+	 * @param obj		The object to convert.
+	 * @return			The string.
+	 * 
+	 * @author Benjamin Jakobus
+	 */
+	private static String toString(Object obj) {
+		return (obj == null ? null : obj.toString());
 	}
 }
